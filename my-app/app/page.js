edcec66bd5a4bd63f1d091a113f6ec351639ee91
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-01 05:50:22 +0000

    #

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -2,27 +2,141 @@ import Image from 'next/image';
 
 function HeroSection() {
   return (
-    <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col">
-      
-      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-      
-      <p className="text-center text-white">Transform Your Business with Seamless SaaS Solutions - Elevate Efficiency, Amplify Productivity!</p>
-      
-      <div className="bg-gradient-to-r from-violet-800 to-zinc-50 text-transparent bg-clip-text relative">
-        <h1 className="text-6xl font-bold text-center md:text-[300px]">morsystems</h1>
-      </div>
+    <>
+      <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col">
 
-      <div className="flex justify-center items-center relative md:mt-[-70px]">
-        <Image
-          src="/assets/preview.png"
-          alt="banner image"
-          height={1200}
-          width={1200}
-          className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
-        />
-        <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
-      </div>
-    </section>
+        <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+
+        <p className="text-center text-white">Transform Your Business with Seamless SaaS Solutions - Elevate Efficiency, Amplify Productivity!</p>
+
+        <div className="bg-gradient-to-r from-violet-800 to-zinc-50 text-transparent bg-clip-text relative">
+          <h1 className="text-6xl font-bold text-center md:text-[300px]">morsystems</h1>
+        </div>
+
+        <div className="flex justify-center items-center relative md:mt-[-70px]">
+          <Image
+            src="/assets/preview.png"
+            alt="banner image"
+            height={1200}
+            width={1200}
+            className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
+          />
+          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+        </div>
+      </section>
+      <section className="flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px]">
+        <h2 className="text-4xl text-center"> Choose what fits you right</h2>
+        <p className="text-muted-foreground text-center">
+          Our straightforward pricing plans are tailored to meet your needs. If
+          {" you're"} not <br />
+          ready to commit you can get started for free.
+        </p>
+        <div className="flex  justify-center gap-4 flex-wrap mt-6">
+          {prices.data.map((card) => (
+            //WIP: Wire up free product from stripe
+            <Card
+              key={card.nickname}
+              className={clsx('w-[300px] flex flex-col justify-between', {
+                'border-2 border-primary': card.nickname === 'Unlimited Saas',
+              })}
+            >
+              <CardHeader>
+                <CardTitle
+                  className={clsx('', {
+                    'text-muted-foreground': card.nickname !== 'Unlimited Saas',
+                  })}
+                >
+                  {card.nickname}
+                </CardTitle>
+                <CardDescription>
+                  {
+                    pricingCards.find((c) => c.title === card.nickname)
+                      ?.description
+                  }
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="text-4xl font-bold">
+                  {card.unit_amount && card.unit_amount / 100}
+                </span>
+                <span className="text-muted-foreground">
+                  <span>/ {card.recurring?.interval}</span>
+                </span>
+              </CardContent>
+              <CardFooter className="flex flex-col items-start gap-4">
+                <div>
+                  {pricingCards
+                    .find((c) => c.title === card.nickname)
+                    ?.features.map((feature) => (
+                      <div
+                        key={feature}
+                        className="flex gap-2"
+                      >
+                        <Check />
+                        <p>{feature}</p>
+                      </div>
+                    ))}
+                </div>
+                <Link
+                  href={`/agency?plan=${card.id}`}
+                  className={clsx(
+                    'w-full text-center bg-primary p-2 rounded-md',
+                    {
+                      '!bg-muted-foreground':
+                        card.nickname !== 'Unlimited Saas',
+                    }
+                  )}
+                >
+                  Get Started
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+          <Card className={clsx('w-[300px] flex flex-col justify-between')}>
+            <CardHeader>
+              <CardTitle
+                className={clsx({
+                  'text-muted-foreground': true,
+                })}
+              >
+                {pricingCards[0].title}
+              </CardTitle>
+              <CardDescription>{pricingCards[0].description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <span className="text-4xl font-bold">$0</span>
+              <span>/ month</span>
+            </CardContent>
+            <CardFooter className="flex flex-col  items-start gap-4 ">
+              <div>
+                {pricingCards
+                  .find((c) => c.title === 'Starter')
+                  ?.features.map((feature) => (
+                    <div
+                      key={feature}
+                      className="flex gap-2"
+                    >
+                      <Check />
+                      <p>{feature}</p>
+                    </div>
+                  ))}
+              </div>
+              <Link
+                href="/agency"
+                className={clsx(
+                  'w-full text-center bg-primary p-2 rounded-md',
+                  {
+                    '!bg-muted-foreground': true,
+                  }
+                )}
+              >
+                Get Started
+              </Link>
+            </CardFooter>
+          </Card>
+        </div>
+      </section>
+    </>
   );
 }
 
