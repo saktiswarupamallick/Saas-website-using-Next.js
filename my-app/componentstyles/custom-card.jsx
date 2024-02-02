4488feae7aa64eec81f9aadec4d01c15eefc00f4
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-02 15:03:31 +0000

    #

--- my-app/componentstyles/custom-card.jsx
+++ my-app/componentstyles/custom-card.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, CardFooter, CardHeader } from '../ui/card';
+import { Card, CardContent, CardFooter, CardHeader } from './card';
 
 
 const CustomCard = ({
